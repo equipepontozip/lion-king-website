5f9811208ded5450e99a34452176f43950b57875
matheusbsilva <matheusbattista@hotmail.com>
2019-07-24 16:00:42 -0300

    Implementação básica de keyup e keydown no js

--- static/login/login.js
+++ static/login/login.js
@@ -1,3 +1,34 @@
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+'use strict';
+// O time é em ms
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  let buffer = []
+  let lastKeyTimeDown = Date.now()
+  let lastKeyTimeUp = Date.now()
+  let lastKeyTimePress = Date.now()
+
+  document.addEventListener('keydown', event => {
+    const key = event.key;
+    const currentTime = Date.now();
+
+    const timeDiff = currentTime - lastKeyTimeDown;
+    lastKeyTimeDown = currentTime;
+
+    console.log("KeyDown: " + [key, timeDiff])
+    
+  });
+
+
+  document.addEventListener('keyup', event => {
+    const key = event.key;
+    const currentTime = Date.now();
+
+    const timeDiff = currentTime - lastKeyTimeUp;
+    lastKeyTimeUp = currentTime;
+
+    console.log("KeyUp: " + [key, timeDiff])
+    
+  });
+
 });
