8c4d063cf1415bad9635fdde8402f603af248132
matheusbsilva <matheusbattista@hotmail.com>
2019-07-24 18:19:20 -0300

    configura para pegar ud e dd para subir no modelo

--- static/login/login.js
+++ static/login/login.js
@@ -3,24 +3,48 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  let buffer = []
-  let lastKeyTimeDown = Date.now()
-  let lastKeyTimeUp = Date.now();
+  let data = {}
+
+  let lastKeyDown = {
+    key: "",
+    time: Date.now()
+  };
+
+  let lastKeyUp= {
+    key: "",
+    time: Date.now()
+  };
 
   document.addEventListener('keydown', event => {
     const key = event.key;
     const currentTime = Date.now();
 
-    const dd = currentTime - lastKeyTimeDown;
-    lastKeyTimeDown = currentTime;
+    const dd = currentTime - lastKeyDown.time;
+
+    lastKeyDown.key = key;
+    lastKeyDown.time = currentTime;
 
     let ud = 0;
-    if(lastKeyTimeUp !== 0) {
-      ud = currentTime - lastKeyTimeUp;
+    if(lastKeyUp.time !== 0) {
+      ud = currentTime - lastKeyUp.time;
     }
 
-    console.log("DD: " + [key, dd])
-    console.log("UD: " + [key, ud])
+    const ddData = {
+      from: lastKeyDown.key,
+      to: key,
+      time: dd/1000,
+      type: 'dd'
+    }
+
+    const udData = {
+      from: lastKeyUp.key,
+      to: key,
+      time: ud/1000,
+      type: 'ud'
+    }
+
+    console.log([ddData.from, ddData.to, ddData.time, ddData.type])
+    console.log([udData.from, udData.to, udData.time, udData.type])
     
   });
 
@@ -29,7 +53,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const key = event.key;
     const currentTime = Date.now();
 
-    lastKeyTimeUp = currentTime;
+    lastKeyUp.key = key;
+    lastKeyUp.time = currentTime;
     
   });
 
